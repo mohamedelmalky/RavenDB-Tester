bd9edac3f8a664599a104275bf6153d683cce52a
Mohamed Elmalky <mohamed.elmalky@gmail.com>
2012-04-05 21:47:21 -0500

    added item and inventory

---
+++ RavenDB Tester/Controllers/InventoryController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RavenDB_Tester.Controllers
+{
+    public class InventoryController : Controller
+    {
+        //
+        // GET: /Inventory/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult AddItem()
+        {
+            return View();
+        }
+    }
+}
---
+++ RavenDB Tester/Models/Item.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RavenDB_Tester.Models
+{
+    public class Item
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+    }
+}
